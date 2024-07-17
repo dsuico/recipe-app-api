79a49ab4de706f1461028073a21d0c1c123efc92
David Suico <dale.alexis96@gmail.com>
2024-07-18 07:26:51 +0800

    fix flake8

--- app/core/admin.py
+++ app/core/admin.py
@@ -44,6 +44,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
-    
+
+
 admin.site.register(models.User, UserAdmin)
-  
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,7 +9,7 @@ from django.test import Client
 
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
-    
+
     def setUp(self):
         """Create user and client."""
         self.client = Client()
@@ -23,25 +23,22 @@ class AdminSiteTests(TestCase):
             password='password',
             name='test user'
         )
-        
+
     def test_users_list(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-        
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-        
+
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-        
         self.assertEqual(res.status_code, 200)
-        
+
     def test_create_user_page(self):
         """Test the create user page works."""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-        
         self.assertEqual(res.status_code, 200)
